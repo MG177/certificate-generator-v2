f36f3388f67922122a683e8180ca282981c341fa
MG177 <mg.terok77@gmail.com>
2025-09-21 16:19:21 +0700

    fix

--- components/email/index.ts
+++ components/email/index.ts
@@ -3,7 +3,6 @@ export {
   EmailStatusBadge,
   EmailStatusTooltip,
 } from './email-status-indicator';
-export { EmailConfigDialog } from './email-config-dialog';
 export { EmailConfigView } from './email-config-view';
 export { BulkEmailActions } from './bulk-email-actions';
 export { EmailStatusDashboard } from './email-status-dashboard';
